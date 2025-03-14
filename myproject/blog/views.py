bcdaf6c203b8c47fa49b3314cee63aae58bb59cc
vishnukumar4224 <vishnuwaiting4urmail@gmail.com>
2025-03-14 15:24:58 +0530

    showed the database data on the index.html page

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,19 +1,20 @@
 import logging
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from .models import post
 # Create your views here.
 
-posts = [
-        {'id':1 ,'title': 'post 1', 'content': 'content of post 1'},
-        {'id':2 ,'title': 'post 2', 'content': 'content of post 2'},
-        {'id':3 ,'title': 'post 3', 'content': 'content of post 3'},
-        {'id':4 ,'title': 'post 4', 'content': 'content of post 4'}
-    ]
+# Static demo data
+# posts = [
+#         {'id':1 ,'title': 'post 1', 'content': 'content of post 1'},
+#         {'id':2 ,'title': 'post 2', 'content': 'content of post 2'},
+#         {'id':3 ,'title': 'post 3', 'content': 'content of post 3'},
+#         {'id':4 ,'title': 'post 4', 'content': 'content of post 4'}
+#     ]
 
 def index(request):
     blog_title = "Latest Posts"
-
+    posts = post.objects.all()
     return render(request, 'blog/index.html', {'title': blog_title, 'posts': posts})
 
 def detail(request, post_id):
