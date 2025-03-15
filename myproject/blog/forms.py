aa77ca3298cab3b63d4616de576695fecfd87288
vishnukumar4224 <vishnuwaiting4urmail@gmail.com>
2025-03-15 15:57:16 +0530

    registration form if the form is incomplete it shows the filled value after submition, so that we can easily continue the existing feild

--- myproject/blog/forms.py
+++ myproject/blog/forms.py
@@ -1,6 +1,21 @@
 from django import forms
+from django.contrib.auth.models import User
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=100, label="name")
-    email = forms.EmailField(label="email")
-    message = forms.CharField(label="message")
+    name = forms.CharField(max_length=100, label="name", required=True)
+    email = forms.EmailField(label="email", required=True)
+    message = forms.CharField(label="message", required=True)
+
+
+class RegisterForm(forms.ModelForm):
+    username = forms.CharField(max_length=100, label="Username", required=True)
+    email = forms.EmailField(label="Email", required=True)
+    password = forms.CharField(label="Password", required=True, max_length=100)
+    confirm_password = forms.CharField(label="Confirm Password", required=True, max_length=100)
+
+
+    #User model is already defined in django so we are using it. By just importing it and we customize that model by following way.
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+        
