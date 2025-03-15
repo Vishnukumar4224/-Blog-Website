17f29f9615f25f1830fa8133163861f04190f011
vishnukumar4224 <vishnuwaiting4urmail@gmail.com>
2025-03-15 14:59:55 +0530

    customize admin interface

--- myproject/blog/admin.py
+++ myproject/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import post, Category
 # Register your models here.
+
+
+#customizing the admin panel
+class postAdmin(admin.ModelAdmin):
+    list_display = ('title','content')
+    search_fields = ('title','content')
+    list_filter = ('category','createdAt')
+
+    def category(self,obj):
+        return 
+
+
+admin.site.register(post,postAdmin)
+admin.site.register(Category)
